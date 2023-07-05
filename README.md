3cda71e4623c9e937654da41374b1515bf235ac9
Álan Crístoffer <acristoffers@startmail.com>
2023-07-05 16:31:17 +0200

    chore: update README and about

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # A beautifier for MATLAB files.
 
-This beautifier is quite opinionated and does not offer many options. It also
-loves to eat comments.
+This beautifier is quite opinionated and does not offer many options. It
+formats code to 4 spaces and aligns matrices cells. It lets you chose if you
+want spaces around all operators or only around addition/subtraction. For now
+(and possibly forever) that's all the options you have.
--- src/args.rs
+++ src/args.rs
@@ -10,8 +10,10 @@ pub use clap::Parser;
 static LONG_ABOUT: &str = "
 matlab-beautifier formats and beautifies MATLAB(R) code.
 
-This beautifier is quite opinionated and does not offer many options. It also
-loves to eat comments.";
+This beautifier is quite opinionated and does not offer many options. It
+formats code to 4 spaces and aligns matrices cells. It lets you chose if you
+want spaces around all operators or only around addition/subtraction. For now
+(and possibly forever) that's all the options you have.";
 
 #[derive(Debug, Parser)]
 #[command(author, version, about = LONG_ABOUT)]
