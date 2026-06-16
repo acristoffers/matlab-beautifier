8a8d69613c7e9b38f1122d6e155f018f87fe7a6a
Álan Crístoffer <acristoffers@startmail.com>
2026-06-16 09:08:40 +0200

    release: version 1.0.3

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        version = "1.0.2";
+        version = "1.0.3";
         pkgs = (import nixpkgs) { inherit system; };
         nativeBuildInputs = with pkgs; [ cmake pkg-config rustc cargo ];
         buildInputs = [ ];
